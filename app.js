7c6600da482cf1ead372f147c34ad7b4443785e4
JauniKappauni <social.jauni@gmail.com>
2025-03-21 17:59:53 +0100

    Added POST route to handle note submission and save it in memory Added use of middleware configuration Added empty list named notes Added return of notes to the GET route of the index page

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let notes = []
+
 app.set('view engine', 'ejs')
-express.urlencoded( {extended: true})
+app.use(express.urlencoded( {extended: true}))
 
 app.get('/', (req, res) => {
-    res.render('index', { notes: [] });
+    res.render('index', { notes: notes });
+});
+
+app.post('/', (req, res) => {
+    const newNote = req.body.note;
+    notes.push(newNote);
+    res.redirect('/')
 });
 
 
