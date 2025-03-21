98d7dd2c778a7cdc9c12f4e274c813c4de7233cb
JauniKappauni <social.jauni@gmail.com>
2025-03-21 17:50:01 +0100

    Added middleware to parse URL-encoded form data

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
+express.urlencoded( {extended: true})
 
 app.listen(port, () => console.log(`Server läuft auf http://localhost:${port}`))
