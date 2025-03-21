e542b614bc91b70bf2f2b00d3c904e3fb511e00f
JauniKappauni <social.jauni@gmail.com>
2025-03-21 17:50:38 +0100

    Added GET route to render the index page with EJS

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const port = 3000
 app.set('view engine', 'ejs')
 express.urlencoded( {extended: true})
 
+app.get('/', (req, res) => {
+    res.render('index', { notes: [] });
+});
+
+
 app.listen(port, () => console.log(`Server läuft auf http://localhost:${port}`))
